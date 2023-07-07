2561ba32142f4b7a6eb9b73b3ec16a401f95efed
Johannes Kristan <Johannes.kristan@bosch.com>
2023-07-07 17:11:44 +0200

    Fix Regex in SparqlQueries
    Signed-off-by: Johannes Kristan <Johannes.kristan@bosch.com>

--- backend/src/main/java/org/eclipse/tractusx/semantics/hub/persistence/triplestore/SparqlQueries.java
+++ backend/src/main/java/org/eclipse/tractusx/semantics/hub/persistence/triplestore/SparqlQueries.java
@@ -58,17 +58,12 @@ public class SparqlQueries {
 
    public static final String ASPECT = "aspect";
    public static final String STATUS_RESULT = "statusResult";
-   public static final String SAMM_ASPECT_URN_REGEX = "(urn:samm:org.eclipse.esmf.samm:meta-model:\\d\\.\\d\\.\\d#Aspect)?(urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#Aspect)?";
-   public static final String ALL_SAMM_ASPECT_URN_PREFIX = "(urn:samm:org.eclipse.esmf.samm:([a-z]|-)+:\\d\\.\\d\\.\\d#)?(urn:bamm:io.openmanufacturing:([a-z]|-)+:\\d\\.\\d\\.\\d#)?";
-   public static final String SAMM_ASPECT_URN_PREFIX = "(urn:samm:org.eclipse.esmf.samm:meta-model:\\d\\.\\d\\.\\d#)?(urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#)?";
+   public static final String SAMM_ASPECT_URN_REGEX = "(urn:samm:org.eclipse.esmf.samm:meta-model:\\d\\.\\d\\.\\d#Aspect)|(urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#Aspect)";
+   public static final String ALL_SAMM_ASPECT_URN_PREFIX = "(urn:samm:org.eclipse.esmf.samm:([a-z]|-)+:\\d\\.\\d\\.\\d#)|(urn:bamm:io.openmanufacturing:([a-z]|-)+:\\d\\.\\d\\.\\d#)";
+   public static final String SAMM_ASPECT_URN_PREFIX = "(urn:samm:org.eclipse.esmf.samm:meta-model:\\d\\.\\d\\.\\d#)|(urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#)";
    public static final String SAMM_PREFERRED_NAME = "urn:samm:org.eclipse.esmf.samm:meta-model:1.0.0#preferredName";
    public static final String SAMM_DESCRIPTION = "urn:samm:org.eclipse.esmf.samm:meta-model:1.0.0#description";
 
-   public static final String BAMM_ASPECT_URN_REGEX = "urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#Aspect";
-   public static final String ALL_BAMM_ASPECT_URN_PREFIX = "urn:bamm:io.openmanufacturing:([a-z]|-)+:\\d\\.\\d\\.\\d#";
-   public static final String BAMM_ASPECT_URN_PREFIX = "urn:bamm:io.openmanufacturing:meta-model:\\d\\.\\d\\.\\d#";
-   public static final String BAMM_PREFERRED_NAME = "urn:bamm:io.openmanufacturing:meta-model:1.0.0#preferredName";
-   public static final String BAMM_DESCRIPTION = "urn:bamm:io.openmanufacturing:meta-model:1.0.0#description";
    public static final Property STATUS_PROPERTY = ResourceFactory.createProperty( AUXILIARY_NAMESPACE, "status" );
 
    private static final String DELETE_BY_URN_QUERY =
@@ -276,8 +271,6 @@ public class SparqlQueries {
     * The simple query needs 50 milliseconds for the same dataset.
     *
     * @param namespaceFilter searches for any namespace matching this parameter
-    * @param nameFilter matches the content of preferredName
-    * @param nameType matches the name (e.g. Property, Characteristic)
     * @param status matches the package status
     * @param page the page to retrieve
     * @param pageSize the page size
