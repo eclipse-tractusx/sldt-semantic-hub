f7f2e5254c506818e84db4ae36fa315db30a61f3
mittal <mittal.vaghela@smartsensesolutions.com>
2024-12-19 19:07:15 +0530

    Update jena version 5.0.0 Add http client library

--- backend/src/main/java/org/eclipse/tractusx/semantics/hub/TripleStoreConfiguration.java
+++ backend/src/main/java/org/eclipse/tractusx/semantics/hub/TripleStoreConfiguration.java
@@ -67,7 +67,6 @@ public class TripleStoreConfiguration {
                          .verbose( true )
                          .contextPath( embedded.getContextPath() )
                          .enableStats( true )
-                         .enableCors( true )
                          .build().start();
    }
 
